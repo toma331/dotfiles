ec4d83e10c25bb227f6bd1151c798d88ff4fbfe1
bossy <bossy@debian>
2026-01-27 02:54:46 +0300

    add dotfiles

---
+++ .emacs.d/init.el
@@ -0,0 +1,44 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq ring-bell-function 'ignore)
+
+(setq make-backup-files nil)   ; Stop creating ~ backup files
+(setq auto-save-default nil)   ; Stop creating #autosave# files
+
+(global-display-line-numbers-mode 1)
+;; (setq display-line-numbers 'relative)
+
+(setq inhibit-startup-screen t)   ; Disable startup screen
+(setq initial-scratch-message "") ; Empty scratch buffer
+
+;; Tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+(set-face-attribute 'default nil :font "IosevkaNerdFont-14.1")
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20"
+     "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7"
+     "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d"
+     "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b"
+     "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970"
+     default))
+ '(package-selected-packages '(gruber-darker-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
