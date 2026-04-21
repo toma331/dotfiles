790bef76eb00a52911c2bb9e4c949763336e1c86
bossy228 <arthom.koval@gmail.com>
2026-04-21 20:33:24 +0300

    Update autostart.sh

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -5,7 +5,6 @@ xset b off
 ~/scripts/switch_mode.sh
 polybar &
 ~/.fehbg
-bash /home/bossy/.config/polybar/launch.sh --grayblocks
 setxkbmap -layout us,ru -option grp:caps_toggle
 xsetroot -cursor_name left_ptr
 picom --backend glx --vsync -b
